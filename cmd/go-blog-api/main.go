f57fce34d6d6d25cd1da244c2ad43e879679de25
Chris <me@lifeoncruzcontrol.com>
2024-12-25 22:56:52 -0800

    Starting with all code in main to implement functionality, then refactoring to separate out logic

--- cmd/go-blog-api/main.go
+++ cmd/go-blog-api/main.go
@@ -4,7 +4,23 @@ import (
 	"fmt"
 
 	"net/http"
+
+	"github.com/google/uuid"
+
+	"time"
 )
+
+type post struct {
+	ID     uuid.UUID `json:"id"`
+	UserId uuid.UUID `json:"userid"`
+	Text   string    `json:"text"`
+	Date   time.Time `json:"date"`
+}
+
+type user struct {
+	UserId uuid.UUID `json:"id"`
+	Name   string    `json:"name"`
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
