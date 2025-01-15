5afcbc9fa7781b66e64ecfbc86bfc5a5d4e41422
Chris <me@lifeoncruzcontrol.com>
2025-01-14 19:05:03 -0800

    Moved entities to their own folder for better modularity

---
+++ entities/post.go
@@ -0,0 +1,18 @@
+package entities
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Post struct {
+	ID       uuid.UUID `json:"id"`
+	Username string    `json:"Username"`
+	Text     string    `json:"text"`
+	Datetime time.Time `json:"datetime"`
+}
+
+type UpdateText struct {
+	Text string `json:"text"`
+}
