e85b2ab2b80b798cce301cb4819a503fe3a859d9
Chris <chriscruz9000@proton.me>
2025-01-23 12:13:12 -0800

    Updated GetPostByID to properly query collection using the hex string returned from create post. Updated CreatePost to initialize newPost with a NewObjectID, as well as return a hex string of the ObjectID. Commented out patch and delete since WIP

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -11,6 +11,10 @@ import (
 	"go-blog-api/entities"
 	"go-blog-api/storage"
 	"go-blog-api/utils"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func GetAllPostsHandler(w http.ResponseWriter) {
@@ -30,15 +34,40 @@ func GetPostByIDHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	post, exists := storage.PostsMap[id]
+	// Validate if the provided ID is in a valid hex format for MongoDB ObjectID
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		http.Error(w, "Invalid ID format", http.StatusBadRequest)
+		return
+	}
 
-	if !exists {
-		http.Error(w, "Post not found", http.StatusNotFound)
+	// Use the valid ObjectID to filter the query
+	filter := bson.M{"_id": objectID}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	var res entities.Post
+	err = db.Posts.FindOne(ctx, filter).Decode(&res)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			log.Printf("Post not found for id: %s", id)
+			http.Error(w, "Post not found", http.StatusNotFound)
+		} else {
+			log.Printf("Error querying post: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+		}
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(post)
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		// Log any JSON encoding error
+		log.Printf("Error encoding response: %v", err)
+
+		// Return a 500 error if response encoding fails
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 }
 
 func CreatePostHandler(w http.ResponseWriter, r *http.Request) {
@@ -48,6 +77,8 @@ func CreatePostHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
+	// Initialize the ID field with a new ObjectID
+	newPost.ID = primitive.NewObjectID()
 	newPost.Datetime = time.Now()
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
@@ -57,8 +88,15 @@ func CreatePostHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to save post", http.StatusInternalServerError)
 		return
 	}
+	// Convert the inserted ID to a string (hex format) for client use
+	insertedID, ok := doc.InsertedID.(primitive.ObjectID)
+	if !ok {
+		log.Printf("Error converting inserted ID to ObjectID")
+		http.Error(w, "Error generating post ID", http.StatusInternalServerError)
+		return
+	}
 	res := entities.PostResponse{
-		ID:       doc.InsertedID,
+		ID:       insertedID.Hex(),
 		Datetime: newPost.Datetime,
 	}
 	w.Header().Set("Content-Type", "application/json")
@@ -73,42 +111,42 @@ func CreatePostHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func PatchTextByIdHandler(w http.ResponseWriter, r *http.Request) {
-	urlVars := r.URL.Query()
-	id := urlVars.Get("id")
-	if id == "" {
-		http.Error(w, "ID missing", http.StatusBadRequest)
-		return
-	}
-	post, exist := storage.PostsMap[id]
-	if !exist {
-		http.Error(w, "Post does not exist for that ID", http.StatusNotFound)
-		return
-	}
-	var updatedText entities.UpdateText
-	if err := utils.DecodeJSON(r, &updatedText); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	post.Text = updatedText.Text
-	storage.PostsMap[id] = post
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(post)
-}
+// func PatchTextByIdHandler(w http.ResponseWriter, r *http.Request) {
+// 	urlVars := r.URL.Query()
+// 	id := urlVars.Get("id")
+// 	if id == "" {
+// 		http.Error(w, "ID missing", http.StatusBadRequest)
+// 		return
+// 	}
+// 	post, exist := storage.PostsMap[id]
+// 	if !exist {
+// 		http.Error(w, "Post does not exist for that ID", http.StatusNotFound)
+// 		return
+// 	}
+// 	var updatedText entities.UpdateText
+// 	if err := utils.DecodeJSON(r, &updatedText); err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+// 	post.Text = updatedText.Text
+// 	storage.PostsMap[id] = post
+// 	w.Header().Set("Content-Type", "application/json")
+// 	json.NewEncoder(w).Encode(post)
+// }
 
-func DeletePostByIdHandler(w http.ResponseWriter, r *http.Request) {
-	urlVars := r.URL.Query()
-	id := urlVars.Get("id")
-	if id == "" {
-		http.Error(w, "ID missing", http.StatusBadRequest)
-		return
-	}
-	_, exist := storage.PostsMap[id]
+// func DeletePostByIdHandler(w http.ResponseWriter, r *http.Request) {
+// 	urlVars := r.URL.Query()
+// 	id := urlVars.Get("id")
+// 	if id == "" {
+// 		http.Error(w, "ID missing", http.StatusBadRequest)
+// 		return
+// 	}
+// 	_, exist := storage.PostsMap[id]
 
-	if !exist {
-		http.Error(w, "Post does not exist for that ID", http.StatusNotFound)
-		return
-	}
-	delete(storage.PostsMap, id)
-	w.WriteHeader(http.StatusNoContent)
-}
+// 	if !exist {
+// 		http.Error(w, "Post does not exist for that ID", http.StatusNotFound)
+// 		return
+// 	}
+// 	delete(storage.PostsMap, id)
+// 	w.WriteHeader(http.StatusNoContent)
+// }
