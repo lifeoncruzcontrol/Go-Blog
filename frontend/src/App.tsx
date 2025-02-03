34b88d04ea1ce3b560197db97c3a63d210aba4a3
Chris <chriscruz9000@proton.me>
2025-02-03 00:07:16 -0800

    Moved router to App.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar';
 import { TextField } from '@mui/material';
+import Home from './pages/Home';
+import History from './pages/History';
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <Router>
       <Navbar />
-      <TextField />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+    </Router>
+    <TextField />
     </>
   )
 }
