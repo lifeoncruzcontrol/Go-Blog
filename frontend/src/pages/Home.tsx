59703612ed6b4fc7092f25c5f330636d98cd53b4
Chris <chriscruz9000@proton.me>
2025-02-03 11:25:18 -0800

    Changed button text

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -37,7 +37,7 @@ const Home: React.FC = () => {
                     }
                 }}
             >
-                Submit
+                Create new post
             </Button>
         </Box>
     );
