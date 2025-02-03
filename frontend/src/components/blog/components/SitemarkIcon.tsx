1e7c5c9dd2f2ebe5a390f17aa0af98b77a15c7b8
Chris <chriscruz9000@proton.me>
2025-02-02 16:19:52 -0800

    Restarting frontend from scratch

--- frontend/src/components/blog/components/SitemarkIcon.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import logo from '../../../assets/logo.jpg';
-
-export default function SitemarkIcon() {
-  return <img src={logo} alt="Site Logo" width={70} height={70} />;
-}
